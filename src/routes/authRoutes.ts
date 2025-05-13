26f030f68503cc263e1bb76daaaa021ff15f9bb6
tnanthagopal <ruby.nanthagopal@gmail.com>
2025-05-13 19:20:59 +0530

    Added authentication and authorization

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { logIn, logout, refresh } from "../controllers/authControllers";
+import { logIn, refresh } from "../controllers/authControllers";
 const router: Router = Router();
 
 router.post("/login", logIn);
 router.post("/refresh", refresh);
-router.post("/logout", logout);
 
 export default router;
